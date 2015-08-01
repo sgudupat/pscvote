bdf28bff3d019c9bc54ddca86c8f547eda15ec8f
murali333 <murali.n333@gmail.com>
2015-08-01 18:44:25 +0530

    registration page code

--- src/com/psc/vote/app/SignUpPageActivity.java
+++ src/com/psc/vote/app/SignUpPageActivity.java
@@ -3,6 +3,10 @@ package com.psc.vote.app;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.RadioButton;
+import android.widget.Spinner;
 
 public class SignUpPageActivity extends Activity {
 
@@ -11,5 +15,35 @@ public class SignUpPageActivity extends Activity {
         super.onCreate(savedInstanceState);
         Log.i("SignUpPageActivity", "inside user registration page");
         setContentView(R.layout.sign_up);
+        
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+     // Create an ArrayAdapter using the string array and a default spinner layout
+     ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+             R.array.age_array, android.R.layout.simple_spinner_item);
+     // Specify the layout to use when the list of choices appears
+     adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+     // Apply the adapter to the spinner
+     spinner.setAdapter(adapter);
+    }
+    
+    public void onCheckboxClicked(View view) {
+        // Is the view now checked?
+       
+    }
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+        
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.radio_pirates:
+                if (checked)
+                    // Pirates are the best
+                break;
+            case R.id.radio_ninjas:
+                if (checked)
+                    // Ninjas rule
+                break;
+        }
     }
 }
