1789a96c83335a631078a1e1bf903438226f314c
sgudupat <sandeep.g@pscglobalsolutions.com>
2015-08-03 17:07:33 +0530

    Added Server access for User registration

--- src/com/psc/vote/app/SignUpPageActivity.java
+++ src/com/psc/vote/app/SignUpPageActivity.java
@@ -4,9 +4,11 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.RadioButton;
-import android.widget.Spinner;
+import android.widget.*;
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.util.ArrayList;
 
 public class SignUpPageActivity extends Activity {
 
@@ -24,14 +26,9 @@ public class SignUpPageActivity extends Activity {
         spinner.setAdapter(adapter);
     }
 
-    public void onCheckboxClicked(View view) {
-        // Is the view now checked?
-    }
-
     public void onRadioButtonClicked(View view) {
         // Is the button now checked?
         boolean checked = ((RadioButton) view).isChecked();
-
         // Check which radio button was clicked
         switch (view.getId()) {
             case R.id.radio_pirates:
@@ -42,6 +39,48 @@ public class SignUpPageActivity extends Activity {
                 if (checked)
                     // Ninjas rule
                     break;
+        }
+    }
+
+    public void register(View view) {
+        EditText username = (EditText) findViewById(R.id.fld_username);
+        EditText mobile = (EditText) findViewById(R.id.fld_mbl);
+        EditText password = (EditText) findViewById(R.id.signUpPwd);
+        RadioGroup gender = (RadioGroup) findViewById(R.id.myRadioGroup);
+        RadioButton genderButton = (RadioButton) findViewById(gender.getCheckedRadioButtonId());
+        String genderValue = (String) genderButton.getText();
+
+        Spinner age = (Spinner) findViewById(R.id.spinner);
+        String ageValue = age.getSelectedItem().toString();
+
+        Log.i("register:", "register");
+        final ArrayList<NameValuePair> postParameters = new ArrayList<NameValuePair>();
+        postParameters.add(new BasicNameValuePair("username", username.getText().toString()));
+        postParameters.add(new BasicNameValuePair("password", password.getText().toString()));
+        postParameters.add(new BasicNameValuePair("mobile", mobile.getText().toString()));
+        postParameters.add(new BasicNameValuePair("gender", genderValue));
+        postParameters.add(new BasicNameValuePair("age", ageValue));
+        final String response = null;
+        try {
+            Log.i("register", "try");
+            new Thread(new Runnable() {
+                public void run() {
+                    Log.i("Response 2:", "In New Thread");
+                    try {
+                        String response2 = SimpleHttpClient.executeHttpPost("/register", postParameters);
+                        Log.i("Response 2:", response2);
+                    } catch (Exception e) {
+                        Log.i("Response 2:Error:", e.getMessage());
+                    }
+                }
+            }).start();
+            Log.i("register", "call done");
+            String res = response.toString();
+            Log.i("register", res);
+            String resp = res.replaceAll("\\s+", "");
+            Log.i("register", resp);
+        } catch (Exception e) {
+            Log.e("register", e.getMessage());
         }
     }
 }
--- src/com/psc/vote/app/SimpleHttpClient.java
+++ src/com/psc/vote/app/SimpleHttpClient.java
@@ -1,6 +1,3 @@
-/**
- *
- */
 package com.psc.vote.app;
 
 import android.os.StrictMode;
@@ -9,7 +6,6 @@ import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.conn.params.ConnManagerParams;
 import org.apache.http.impl.client.DefaultHttpClient;
@@ -19,16 +15,18 @@ import org.apache.http.params.HttpParams;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.URI;
 import java.util.ArrayList;
-import java.util.Map;
 
 public class SimpleHttpClient {
-    /** The time it takes for our client to timeout */
+    /**
+     * The time it takes for our client to timeout
+     */
     public static final int HTTP_TIMEOUT = 30 * 1000; // milliseconds
     private static String serverURL = "http://52.74.54.79:8080/vote";
 
-    /** Single instance of our HttpClient */
+    /**
+     * Single instance of our HttpClient
+     */
     private static HttpClient mHttpClient;
 
     /**
@@ -51,35 +49,36 @@ public class SimpleHttpClient {
      * Performs an HTTP Post request to the specified url with the
      * specified parameters.
      *
+     * @param submitRequest
      * @param postParameters The parameters to send via the request
      * @return The result of the request
      * @throws Exception
      */
-    public static String executeHttpPost(ArrayList<NameValuePair> postParameters) throws Exception {
+    public static String executeHttpPost(String submitRequest, ArrayList<NameValuePair> postParameters) throws Exception {
         Log.i("SimpleHttpClient", "inside");
-        String url = serverURL + "/vote/registerpost";
-        System.out.println("url::" + url);
-        System.out.println("postParameters::" + postParameters);
+        String url = serverURL + submitRequest;
+        Log.i("url::", url);
+        Log.i("postParameters::", postParameters.toString());
 
         BufferedReader in = null;
         try {
             HttpClient client = getHttpClient();
-            System.out.println("client::" + client);
+            Log.i("client::", client.toString());
             Log.i("SimpleHttpClient::2", client.toString());
             HttpPost request = new HttpPost(url);
             Log.i("SimpleHttpClient::3", request.toString());
 
             UrlEncodedFormEntity formEntity = new UrlEncodedFormEntity(postParameters);
             Log.i("SimpleHttpClient::4", formEntity.toString());
-            System.out.println("request::" + request);
-            System.out.println("formEntity::" + formEntity);
+            Log.i("request::", request.toString());
+            Log.i("formEntity::", formEntity.toString());
             request.setEntity(formEntity);
-            System.out.println("request:2:" + request);
+            Log.i("request:2:", request.toString());
             StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
             StrictMode.setThreadPolicy(policy);
-            System.out.println("request:3:" + "Strict Mode assigned");
+            Log.i("request:3:", "Strict Mode assigned");
             HttpResponse response = client.execute(request);
-            System.out.println("response::" + response.toString());
+            Log.i("response::", response.toString());
             Log.i("SimpleHttpClient::5", response.toString());
             in = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
             Log.i("SimpleHttpClient::6", in.toString());
@@ -108,88 +107,6 @@ public class SimpleHttpClient {
                 } catch (IOException e) {
                     e.printStackTrace();
                     Log.e("SimpleHttpClient", String.valueOf(e));
-                }
-            }
-        }
-    }
-
-    public static String executeHttpPost2() throws Exception {
-        Log.i("executeHttpPost2", "inside");
-        String url = serverURL + "/vote/register?username=%22sandeep%22&password=%22info%22";
-        System.out.println("url::" + url);
-
-        BufferedReader in = null;
-        try {
-            HttpClient client = getHttpClient();
-            HttpPost request = new HttpPost(url);
-            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-            StrictMode.setThreadPolicy(policy);
-            HttpResponse response = client.execute(request);
-            System.out.println("response::" + response);
-            return response.toString();
-        } finally {
-            if (in != null) {
-                try {
-                    in.close();
-                } catch (IOException e) {
-                    Log.e("SimpleHttpClient", String.valueOf(e));
-                }
-            }
-        }
-    }
-
-    /**
-     * Performs an HTTP GET request to the specified url.
-     *
-     * @param url The web address to post the request to
-     * @return The result of the request
-     * @throws Exception
-     */
-    public static String executeHttpGet(String url) throws Exception {
-        BufferedReader in = null;
-        try {
-            HttpClient client = getHttpClient();
-            HttpGet request = new HttpGet();
-            request.setURI(new URI(url));
-            HttpResponse response = client.execute(request);
-            in = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
-
-            StringBuffer sb = new StringBuffer("");
-            String line = "";
-            String NL = System.getProperty("line.separator");
-            while ((line = in.readLine()) != null) {
-                sb.append(line + NL);
-            }
-            in.close();
-
-            String result = sb.toString();
-            return result;
-        } finally {
-            if (in != null) {
-                try {
-                    in.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
-
-    public static Map<String, String> executeHttpGetMap(String url) throws Exception {
-        BufferedReader in = null;
-        try {
-            HttpClient client = getHttpClient();
-            HttpGet request = new HttpGet();
-            request.setURI(new URI(url));
-            HttpResponse response = client.execute(request);
-            Map<String, String> responseInfo = (Map<String, String>) response.getEntity().getContent();
-            return responseInfo;
-        } finally {
-            if (in != null) {
-                try {
-                    in.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
                 }
             }
         }
