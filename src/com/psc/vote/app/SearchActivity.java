ae641343163adbd8ea00326d218a132d4f889104
murali333 <murali.n333@gmail.com>
2015-08-03 19:22:12 +0530

    custom serch view

--- src/com/psc/vote/app/SearchActivity.java
+++ src/com/psc/vote/app/SearchActivity.java
@@ -12,59 +12,122 @@ import android.widget.AdapterView.OnItemClickListener;
 
 public class SearchActivity extends Activity {
 
-    // Listview Adapter
-    ArrayAdapter<String> adapter;
-    // Search EditText
+     // Search EditText
     EditText inputSearch;
-    // List view
-    private ListView lv;
+     
+    ArrayList<Product> items = new ArrayList<Product>();
+     
+ 
+  
+	
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		Log.i("SearchActivity", "inside user sign in page");
+		setContentView(R.layout.search);
+	
+	 String products[] = {"Dell Inspiron", "HTC One X", "HTC Wildfire S", "HTC Sense", "HTC Sensation XE",
+             "iPhone 4S", "Samsung Galaxy Note 800",
+             "Samsung Galaxy S3", "MacBook Air", "Mac Mini", "MacBook Pro"};
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        Log.i("SearchActivity", "inside user sign in page");
-        setContentView(R.layout.search);
+		//lv = (ListView) findViewById(R.id.list_view);
+	  final ListView listView = (ListView) findViewById(R.id.list_view);
+   
 
-        String products[] = {"Dell Inspiron", "HTC One X", "HTC Wildfire S", "HTC Sense", "HTC Sensation XE",
-                "iPhone 4S", "Samsung Galaxy Note 800",
-                "Samsung Galaxy S3", "MacBook Air", "Mac Mini", "MacBook Pro"};
+		
 
-        lv = (ListView) findViewById(R.id.list_view);
-        inputSearch = (EditText) findViewById(R.id.inputSearch);
+			// Adding items to listview
+			//adapter = new ArrayAdapter<String>(this, R.layout.list_item, R.id.product_name, products);
+		 // 1. pass context and data to the custom adapter
+         MyAdapter adapter = new MyAdapter(this, generateData());
+      
+        //2. setListAdapter
+        listView.setAdapter(adapter);
+        
+			//lv.setAdapter(adapter); 
+      	
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        	
+        	
 
-        // Adding items to listview
-        adapter = new ArrayAdapter<String>(this, R.layout.list_item, R.id.product_name, products);
-        lv.setAdapter(adapter);
-
-        lv.setOnItemClickListener(new ListClickHandler());
-
-        /**
-         * Enabling Search Filter
-         * */
-        inputSearch.addTextChangedListener(new TextWatcher() {
-            public void onTextChanged(CharSequence cs, int arg1, int arg2, int arg3) {
-                // When user changed the Text
-                SearchActivity.this.adapter.getFilter().filter(cs);
-            }
-
-            public void beforeTextChanged(CharSequence arg0, int arg1, int arg2, int arg3) {
-                // TODO Auto-generated method stub
-            }
-
-            public void afterTextChanged(Editable arg0) {
-                // TODO Auto-generated method stub
-            }
-        });
-    }
-
-    public class ListClickHandler implements OnItemClickListener {
-        public void onItemClick(AdapterView<?> adapter, View view, int position, long arg3) {
-            // TODO Auto-generated method stub
-            TextView listText = (TextView) view.findViewById(R.id.product_name);
-            String text = listText.getText().toString();
-            Intent intent = new Intent(SearchActivity.this, ShowActivity.class);
-            intent.putExtra("selected-item", text);
-            startActivity(intent);
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				// TODO Auto-generated method stub
+				 TextView listText = (TextView) view.findViewById(R.id.product_name);
+	     	        String text = listText.getText().toString();
+	     	        Intent intent = new Intent(SearchActivity.this, ShowActivity.class);
+	     	        intent.putExtra("selected-item", text);
+	     	        startActivity(intent);  
+				
+			}
         }
-    }
+);
+        	
+        inputSearch = (EditText) findViewById(R.id.inputSearch);
+        Log.i("textchanged","inside on text change");
+			
+			/**
+			* Enabling Search Filter
+			* */
+			inputSearch.addTextChangedListener(new TextWatcher() {
+				
+			
+			public void onTextChanged(CharSequence cs, int arg1, int arg2, int arg3) {
+			// When user changed the Text
+				 int textlength = cs.length();
+				//SearchActivity.this.adapter.getFilter().filter(cs);  
+				ArrayList<Product> temp=new ArrayList<Product>();
+				for(Product p: items){
+					 if (textlength <= p.getPname().length()) {
+		                 if (p.getPname().toLowerCase().contains(cs.toString().toLowerCase())) {
+		                    temp.add(p);
+		                 }
+		              }
+					
+				}
+				
+				MyAdapter adapter1=new MyAdapter(SearchActivity.this,temp);
+				listView.setAdapter(adapter1);
+				
+			}
+			
+			public void beforeTextChanged(CharSequence arg0, int arg1, int arg2,
+			 int arg3) {
+			// TODO Auto-generated method stub
+			
+			}
+			
+			
+			public void afterTextChanged(Editable arg0) {
+			// TODO Auto-generated method stub  
+				
+				
+			}
+			});
+	}
+	
+	
+		 
+	  
+	   
+
+		
+	
+	     
+	
+	 private ArrayList<Product> generateData(){
+	      
+	        items.add(new Product("Dell Inspiron","Five thousand","Dell phone"));
+	        items.add(new Product("HTC One X","Ten thousand","Htc mega pixel"));
+	        items.add(new Product("HTC Wildfire S","Eight thousand","Htc super model"));
+	        items.add(new Product("HTC Sense","Eight thousand","Htc super model"));
+	        items.add(new Product("HTC Sensation XE","Eight thousand","Htc super model"));
+	        items.add(new Product("Samsung Galaxy Note 800","Eight thousand","samsung super model"));
+	        items.add(new Product("Samsung Galaxy S3","Eight thousand","smasung galxy model"));
+	        items.add(new Product("HTC Wildfire S","Eight thousand","samsung  super model"));
+	 
+	        return items;
+	    }
 }
