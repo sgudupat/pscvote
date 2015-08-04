b521d61a4e2ce426fa4afbfb4556f13385bc1af1
murali333 <murali.n333@gmail.com>
2015-08-04 19:15:57 +0530

    send otp sms code

--- src/com/psc/vote/app/SignUpPageActivity.java
+++ src/com/psc/vote/app/SignUpPageActivity.java
@@ -64,26 +64,105 @@ public class SignUpPageActivity extends Activity {
         postParameters.add(new BasicNameValuePair("age", ageValue));
         postParameters.add(new BasicNameValuePair("deviceId", deviceId));
         final String response = null;
-        try {
-            Log.i("register", "try");
-            new Thread(new Runnable() {
-                public void run() {
-                    Log.i("Response 2:", "In New Thread");
-                    try {
-                        String response2 = SimpleHttpClient.executeHttpPost("/register", postParameters);
-                        Log.i("Response 2:", response2);
-                    } catch (Exception e) {
-                        Log.i("Response 2:Error:", e.getMessage());
-                    }
-                }
-            }).start();
-            Log.i("register", "call done");
-            String res = response.toString();
-            Log.i("register", res);
-            String resp = res.replaceAll("\\s+", "");
-            Log.i("register", resp);
-        } catch (Exception e) {
-            Log.e("register", e.getMessage()+"");
-        }
+       
+    
+    final Context context = this;
+    String res;
+	   EditText mobilep = (EditText) findViewById(R.id.fld_mbl);
+	 String chars = "0123456789";
+	 final int PW_LENGTH = 5;
+  Random rnd = new SecureRandom();
+  StringBuilder pass = new StringBuilder();
+  for (int i = 0; i < PW_LENGTH; i++)
+      pass.append(chars.charAt(rnd.nextInt(chars.length())));
+  
+  Log.i("otp password",pass.toString());
+  final String otp=pass.toString();
+  String sms = otp;
+  try {
+ 	 String phoneNo=mobilep.toString();
+			SmsManager smsManager = SmsManager.getDefault();
+			smsManager.sendTextMessage(phoneNo, null, sms, null, null);
+			Toast.makeText(getApplicationContext(), "SMS Sent!",
+						Toast.LENGTH_LONG).show();
+		  } catch (Exception e) {
+			Toast.makeText(getApplicationContext(),
+				"SMS faild, please try again later!",
+				Toast.LENGTH_LONG).show();
+			e.printStackTrace();
+		  }
+
+	// get prompts.xml view
+	LayoutInflater li = LayoutInflater.from(context);
+	View promptsView = li.inflate(R.layout.prompts, null);
+
+	AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+			context);
+
+	// set prompts.xml to alertdialog builder
+	alertDialogBuilder.setView(promptsView);
+
+	final EditText userInput = (EditText) promptsView
+			.findViewById(R.id.editTextDialogUserInput);
+
+	// set dialog message
+	alertDialogBuilder
+		.setCancelable(false)
+		.setPositiveButton("OK",
+		  new DialogInterface.OnClickListener() {
+		    public void onClick(DialogInterface dialog,int id) {
+			// get user input and set it to result
+			// edit text
+			//result.setText(userInput.getText());
+		    
+		    	String aotp=userInput.getText().toString();
+		   	 Log.i("alert dailog otp password",aotp);
+		    	if(aotp.equals(otp)){
+		    		 try {
+		    	          //  Log.i("register", "try");
+		    	            new Thread(new Runnable() {
+		    	                public void run() {
+		    	                   // Log.i("Response 2:", "In New Thread");
+		    	                    try {
+		    	                        String response2 = SimpleHttpClient.executeHttpPost("/register", postParameters);
+		    	                        Log.i("Response from db:", response2);
+		    	                        String res="success";
+		    	                        Log.v("boolean result1",""+TextUtils.equals(response2.toString(), res.toString()));
+		    	                        Log.v("boolean result2",""+TextUtils.equals(response2, "success"));
+		    	                        Log.v("boolean result3",""+res.equals(response2.toString()));
+		    	                        if(TextUtils.equals(response2.toString(), "success")){
+		    		    		    		Intent intent = new Intent(context,SearchActivity.class) ;
+		    		    		            startActivity(intent);
+		    		    		            Log.i("inside otp if loop","search activity started");
+		    		    		    			}
+		    	                        Log.i("Response 2:", response2);
+		    	                    } catch (Exception e) {
+		    	                        Log.i("Response 2:Error:", e.getMessage());
+		    	                    }
+		    	                }
+		    	            }).start();
+		    	          
+		    	        	
+		    	        } catch (Exception e) {
+		    	            Log.e("register", e.getMessage()+"");
+		    	        }
+		    		
+		    		
+		    	}
+		    }
+		  })
+		.setNegativeButton("Cancel",
+		  new DialogInterface.OnClickListener() {
+		    public void onClick(DialogInterface dialog,int id) {
+			dialog.cancel();
+		    }
+		  });
+
+	// create alert dialog
+	AlertDialog alertDialog = alertDialogBuilder.create();
+
+	// show it
+	alertDialog.show();
+    
     }
 }
